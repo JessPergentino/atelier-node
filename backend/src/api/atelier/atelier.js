b25adfb029dd8cf31062704255eb669e81f6290f
JessPergentino <jessicapergentino@hotmail.com>
2018-01-21 13:58:17 -0300

    Ajustes de sintaxe e de modelagem do banco

--- backend/src/api/atelier/atelier.js
+++ backend/src/api/atelier/atelier.js
@@ -17,26 +17,17 @@ const pagamentoSchema = new mongoose.Schema({
     description: {type: String, require: true, enum: ['CARTAO_A_VISTA', 'PARCELADO', 'DEPOSITO_TOTAL','DEPOSITO_50', 'ENTREGA']}
 })
 
-const enderecoSchema = new mongoose.Schema({
-    numero: {type: Number, require:true, min:0},
-    rua: {type: String, require: true},
-    complemento: {type: String, require: false},
-    cidade: {type: String, require: true},
-    estado: {type: String, require: true},
-    cep: {type: String, require: true},
-    bairro: {type: String, require: true}
-})
-
 const clienteSchema = new mongoose.Schema({
     name: {type: String, require: true},
-    endereco: {enderecoSchema}
+    endereco: {type: String, require: true}
 })
 
 const encomendaSchema = new mongoose.Schema({
     ordemServico: {type: String, require: true},
-    santa: {santaSchema},
-    pagamento: {pagamentoSchema},
-    envio: {envioSchema},
+    cliente: clienteSchema,
+    santa: [santaSchema],
+    pagamento: pagamentoSchema,
+    envio: envioSchema,
     createdAt: {type: Date, default: Date.now}
 })
 
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/atelier')
+
+mongoose.Error.messages.general.require = "O atributo '{PATH}' é obrigatorio"
