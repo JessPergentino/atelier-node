3898ebee78d24c87a84762e7fa853541c8686f1a
JessPergentino <jessicapergentino@hotmail.com>
2018-01-22 15:06:47 -0300

    modificação no formulario de encomendas

--- frontend/src/atelier/encomendasForm.jsx
+++ frontend/src/atelier/encomendasForm.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 export default props => (
-    <div>
-        <h1>Form</h1>
+    <div role='form' className='EncomendasForm'>
+ 
+    <div className="form-row">
+    <div className='col-xs-12 col-sm-9 col-md-10'>
+    
+    <label for="validationCustom01">Nome do Cliente</label>
+    <input type="text" id="validationCustom01" placeholder="Insira um nome" required></input>
+
+    <label for="validationCustom01">Nome do Cliente</label>
+    <input type="text" id="validationCustom01" placeholder="Insira um nome" required></input>
+     
     </div>
+     
+    <div className='col-xs-12 col-sm-3 col-md-2'>
+    <button className='btn btn-primary'>
+    <i className='fa fa-plus'></i>
+    </button>
+    </div>
+    </div>
+    </div>   
 )
