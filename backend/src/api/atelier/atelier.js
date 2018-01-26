3c90f8a4bb79b8957594370e1d3a36b9787446b6
JessPergentino <jessicapergentino@hotmail.com>
2018-01-26 10:27:21 -0300

    ajustes na sintaxe do arquivo atelie.js

--- backend/src/api/atelier/atelier.js
+++ backend/src/api/atelier/atelier.js
@@ -2,28 +2,28 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const santaSchema = new mongoose.Schema({
-    name: {type: String, require: true},
-    value: {type: Number, min: 0, require: true},
-    qtd: {type: Number, min:0, require: true},
-    description: {type: String, require: false}
+    name: {type: String, required: true},
+    value: {type: Number, min: 0, required: true},
+    qtd: {type: Number, min:0, required: true},
+    description: {type: String, required: false}
 
 })
 
 const envioSchema = new mongoose.Schema({
-    description: {type: String, require: true, enum: ['PAC','SEDEX','RETIRADA']}
+    description: {type: String, required: true, enum: ['PAC','SEDEX','RETIRADA']}
 })
 
 const pagamentoSchema = new mongoose.Schema({
-    description: {type: String, require: true, enum: ['CARTAO_A_VISTA', 'PARCELADO', 'DEPOSITO_TOTAL','DEPOSITO_50', 'ENTREGA']}
+    description: {type: String, required: true, enum: ['CARTAO_A_VISTA', 'PARCELADO', 'DEPOSITO_TOTAL','DEPOSITO_50', 'ENTREGA']}
 })
 
 const clienteSchema = new mongoose.Schema({
-    name: {type: String, require: true},
-    endereco: {type: String, require: true}
+    name: {type: String, required: true},
+    endereco: {type: String, required: true}
 })
 
 const encomendaSchema = new mongoose.Schema({
-    ordemServico: {type: String, require: true},
+    ordemServico: {type: String, required: true},
     cliente: clienteSchema,
     santa: [santaSchema],
     pagamento: pagamentoSchema,
