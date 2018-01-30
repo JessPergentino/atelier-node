3ef6000aa4ec36449bc1703e453ca55cf2b492a3
JessPergentino <jessicapergentino@hotmail.com>
2018-01-30 09:40:53 -0300

    Fazendo o tratamento de erros

--- backend/src/api/atelier/atelierService.js
+++ backend/src/api/atelier/atelierService.js
@@ -1,7 +1,9 @@
 const Atelier = require('./atelier')
+const errorHandler = require('../common/errorHandler')
 
 Atelier.methods(['get', 'post', 'put', 'delete'])
 Atelier.updateOptions({new: true, runValidators: true})
+Atelier.after('post', errorHandler).after('put', errorHandler)
 
 Atelier.route('count', (req,res,next) => {
     Atelier.count((error,value) => {
